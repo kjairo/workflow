af0a4ccc5eac383589654956148413031e87745e
Giddy <kevinjairo@yahoo.com>
2017-08-05 22:23:06 +0300

    Edit Gulpif and uglified production environment

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var gulp =  require('gulp'),
 	concat = require('gulp-concat'),
 	compass = require('gulp-compass'),
 	browserify = require('gulp-browserify'),
-	connect = require('gulp-connect');
-
+	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify');
+	
 var env,
 	coffeeSources,
 	jsSources,
@@ -48,6 +50,7 @@ gulp.task('js', function(){
 	gulp.src(jsSources)
 	   .pipe(concat('script.js'))
 	   .pipe(browserify())
+	   .pipe(gulpif(env === 'production', uglify()))
 	   .pipe(gulp.dest(outputDir + 'js'))	
 	   .pipe(connect.reload())
 });
